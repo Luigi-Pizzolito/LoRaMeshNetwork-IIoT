4da8e9abfb2c2d8cae07e8a2363fba7f739185a6
Luigi Pizzolito <luigi.pizzolito@hotmail.com>
2023-03-15 11:17:29 +0800

    Added note to update gen.py

--- NodeServer/DBfakedataGen/gen.py
+++ NodeServer/DBfakedataGen/gen.py
@@ -10,31 +10,29 @@ def generate_fake_data(start_date, end_date, time_interval, temp_range, humidity
         temp = round(random.uniform(temp_range[0], temp_range[1]), 2)
         humidity = round(random.uniform(humidity_range[0], humidity_range[1]), 2)
         soil_moisture = round(random.uniform(soil_moisture_range[0], soil_moisture_range[1]), 2)
+        
+        # TODO: restructure this to meet the JSON structure defined in ../JSONformats/reply_format.json
         data[current_time] = {'temperature': temp, 'humidity': humidity, 'soil_moisture': soil_moisture}
+        
         current_date += datetime.timedelta(minutes=time_interval)
     return data
 
-# Example usage
-# start_date = datetime.datetime(2023, 3, 15, 0, 0, 0)
-# end_date = datetime.datetime(2023, 3, 16, 0, 0, 0)
+# get current time and a week back
 end_date = datetime.datetime.now()
 start_date = end_date + datetime.timedelta(days=-7)
 time_interval = 30 # in minutes
+# set fakedata ranges
 temp_range = (15, 30) # in Celsius
 humidity_range = (40, 80) # in %
 soil_moisture_range = (20, 60) # in %
-
-
+# generate fake data
 print('Generating Past Week of Data...')
 print(start_date, "to", end_date)
-
 fake_data = generate_fake_data(start_date, end_date, time_interval, temp_range, humidity_range, soil_moisture_range)
-
+# write fake data to json file
 print('Done, writing data.json')
-
 f = open("data.json", "w")
 f.write(json.dumps(fake_data))
 f.close()
-
 # print(fake_data)
 print('Job Finished.')
