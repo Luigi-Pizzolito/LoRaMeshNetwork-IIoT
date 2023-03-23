666d0ff7f548fd2daa1d01a724d37673f7c7dfd2
spiderbones <127936333+spiderbones@users.noreply.github.com>
2023-03-23 20:46:37 +0800

    added id filter

--- NodeServer/DBbridge.js
+++ NodeServer/DBbridge.js
@@ -4,7 +4,7 @@ class DBBridge {
     //Contructor to initialise database
     constructor(dbpath) {
         this.db = NoSQL.load(dbpath);
-        console.log("DB "+dbpath+" initialised.");
+        console.log("DB " + dbpath + " initialised.");
     }
 
     // Methods for accepting queries
@@ -18,15 +18,27 @@ class DBBridge {
             callback(error, {});
             return;
         }
+
+
         let start_time = queryObj.time_range[0];
         let end_time = queryObj.time_range[1];
         // let sensors = queryObj.sensors; //!TODO: make this iterative filter later if we need it
 
-        this.db.find().make(function(filter) {
-            filter.where('ts', '>', start_time)
-            filter.where('ts', '<', end_time)
-            filter.callback(callback);
-        });
+        if (queryObj.hasOwnProperty('id')) {
+            let id = queryObj.id;
+            this.db.find().make(function (filter) {
+                filter.where('id', '=', id)
+                filter.where('ts', '>', start_time)
+                filter.where('ts', '<', end_time)
+                filter.callback(callback);
+            });
+        } else {
+            this.db.find().make(function (filter) {
+                filter.where('ts', '>', start_time)
+                filter.where('ts', '<', end_time)
+                filter.callback(callback);
+            });
+        }
     }
 
     // Method for adding DB entries
